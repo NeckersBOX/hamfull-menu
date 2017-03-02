08c4538c26c761385ee6d818487f45c9ea8e8018
Davide Francesco Merico <hds619@gmail.com>
2017-03-02 17:09:35 +0100

    Rename minified css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp = require ('gulp');
 const sass = require ('gulp-sass');
 const sourcemaps = require ('gulp-sourcemaps');
+const rename = require ('gulp-rename');
 
 gulp.task ('sass', () =>
   gulp.src ('sass/hamfull.sass')
@@ -11,6 +12,7 @@ gulp.task ('sass', () =>
       outputStyle: 'compressed'
     }).on ('error', sass.logError))
     .pipe (sourcemaps.write ())
+    .pipe (rename ('hamfull.min.css'))
     .pipe (gulp.dest ('css'))
 );
 
